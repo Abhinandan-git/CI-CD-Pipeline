d1497a51655e96c5ffec850a8408361f0ff5f14d
Abhinandan Jain <55753532+Abhinandan-git@users.noreply.github.com>
2025-07-05 11:46:04 +0530

    Feat cicd (#3)
    * Added database access
    
    * Fixed test
    
    * Fixed CI and tests
    
    * Deployment to Render
    
    * README update
    
    * Fix test
    
    * Sample file
    
    * Fixing Pipeline
    
    * Completed frontend
    
    * Frontend complete

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,17 @@
-import Link from "next/link";
+import { getAllTodos } from "@/api";
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
-export default function Page() {
+export default async function Home() {
+  const tasks = await getAllTodos()
+  
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+    <main className="max-w-4xl mx-auto mt-4">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="text-2xl font-bold">To Do List Application</h1>
+        <AddTask />
+      </div>
+      <TodoList tasks={tasks} />
+    </main>
   );
 }
